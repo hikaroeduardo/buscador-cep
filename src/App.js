d90f2dcdbc47a050f902ca99aa68f389e0d81ae2
hikaroeduardo <hikaroeduardobandeira@gmail.com>
2022-12-27 17:27:20 -0300

    Projeto em REACT para buscar endereços através do CEP, consumindo API através do axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import api from "./services/Api/api"
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cepData, setCepData] = useState("");
+
+  async function search() {
+    const responseData = await api.get(`${input}/json`);
+
+    setCepData(responseData.data);
+
+    setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+
+      <div className="content">
+
+        <main>
+
+          <h1>Buscador de CEP</h1>
+
+          <input type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          />
+          <button onClick={search}>Buscar CEP</button>
+
+        </main> {/* main */}
+
+        <section>
+          <h2>Informações da localidade</h2>
+
+          {cepData && (
+
+          <div className="info">
+            <h3>Cep: {cepData.cep}</h3>
+            <p>Logradouro: {cepData.logradouro}</p>
+            <p>Complemento: {cepData.complemento}</p>
+            <p>Bairro: {cepData.bairro}</p>
+            <p>{`${cepData.localidade} - ${cepData.uf}`}</p>
+          </div>
+
+          )}
+
+        </section> {/* section */}
+
+      </div> {/* content */}
+
+    </div> 
+    // container
   );
 }
 
